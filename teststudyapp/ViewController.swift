771bdcdecd5e1e0e99273dd4755add095df67e65
Adam2e <adam2e@live.com>
2019-09-27 12:12:00 -0400

    comment

--- teststudyapp/ViewController.swift
+++ teststudyapp/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor.blue
+        
+        // adam changes things
     }
         
 
